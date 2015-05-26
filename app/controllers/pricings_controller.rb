d761a381d1019bf50bc0080d154d09c745b742f9
bl4ckdu5t <joerexko@gmail.com>
2015-05-26 09:40:27 -0500

    Created pricing routes and pages

---
+++ app/controllers/pricings_controller.rb
@@ -0,0 +1,22 @@
+class PricingsController < ApplicationController
+	def logo
+	end
+
+	def stationery
+	end
+
+	def banners
+	end
+
+	def brochure
+	end
+
+	def website
+	end
+
+	def corporate
+	end
+
+	def custom
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  scope path: "/pricing" do
+    get 'logo'       => 'pricings#logo', as: :pricing_logo
+    get 'stationery' => 'pricings#stationery', as: :pricing_stationery
+    get 'banners'    => 'pricings#banners', as: :pricing_banner
+    get 'brochure'   => 'pricings#brochure', as: :pricing_brochure
+    get 'website'    => 'pricings#website', as: :pricing_website
+    get 'corporate'  => 'pricings#corporate', as: :pricing_corporate
+    get 'custom'     => 'pricings#custom', as: :pricing_custom
+  end
   get 'designer-account' => 'accounts#designers', as: :designer
   get 'customer-account' => 'accounts#customers', as: :customer
   devise_for :users
