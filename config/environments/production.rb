f47dbb8b170acf8397e9b98e389bb127ba2ab355
bl4ckdu5t <joerexko@gmail.com>
2015-06-21 05:21:47 -0500

    Precompiled assets for CDN upload and set asset host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,11 +58,11 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = Proc.new { |source|
-  #   if source =~ /\b(.png|.jpg|.gif)\b/i
-  #     "http://d2witi47u37lbt.cloudfront.net"
-  #   end
-  # }
+  config.action_controller.asset_host = Proc.new { |source|
+    if source =~ /\b(.png|.jpg|.gif)\b/i
+      "http://d2witi47u37lbt.cloudfront.net"
+    end
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
