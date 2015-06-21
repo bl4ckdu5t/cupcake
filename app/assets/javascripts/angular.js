890ebc2a27178ff4d44c61e7a152ad53222d74ec
bl4ckdu5t <joerexko@gmail.com>
2015-06-21 01:15:41 -0500

    Added the OpenGraph protocol to mark-up

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -10,7 +10,7 @@ var app = angular.module('designscook', ['ngCookies','angularUtils.directives.di
   $stateProvider
     .state('step1', {
       url: "/step1",
-      templateUrl: "<h2>Under development</h2>"
+      template: "<h2>Under development</h2>"
     })
     .state('step2', {
       url: "/step2",
