de282e4e5108c8ee71060e4cf583549ab2ac83b7
bl4ckdu5t <joerexko@gmail.com>
2015-07-24 00:55:55 -0500

    Change default URL to absolute designscook.com domain for uploads

--- app/models/brief.rb
+++ app/models/brief.rb
@@ -4,7 +4,8 @@ class Brief < ActiveRecord::Base
 		thumb: '100x100>',
 		square: '200x200#',
 		medium: '300x300>'
-	}, default_url: '/default/no-preview.jpg', storage: :s3, url: ":s3_domain_url", path: "/projects/:id/:style.:extension",
+	}, default_url: 'http://designscook.com/default/no-preview.jpg', storage: :s3, url: ":s3_domain_url", 
+	path: "/projects/:id/:style.:extension",
 	s3_host_name: 's3.amazonaws.com', s3_credentials: "#{Rails.root}/config/aws.yml"
 	# Validate the attached image is image/jpg, image/png, etc
 	validates_attachment_content_type :preview, :content_type => /\Aimage\/.*\Z/
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,12 +5,13 @@ class Submission < ActiveRecord::Base
 		thumb: '100x100>',
 		square: '200x200#',
 		medium: '300x300>'
-	}, default_url: '/default/no-preview.jpg', storage: :s3, url: ":s3_domain_url", path: "/submissions/:id/preview/:style.:extension",
+	}, default_url: 'http://designscook.com/default/no-preview.jpg', storage: :s3, url: ":s3_domain_url",
+	path: "/submissions/:id/preview/:style.:extension",
 	s3_host_name: 's3.amazonaws.com', s3_credentials: "#{Rails.root}/config/aws.yml"
 	# Validate the attached image is image/jpg, image/png, etc
 	validates_attachment_content_type :preview, :content_type => /\Aimage\/.*\Z/
 
-	has_attached_file :project, default_url: '/default/no-preview.jpg', storage: :s3, url: ":s3_domain_url", styles: {
+	has_attached_file :project, default_url: 'http://designscook.com/default/no-preview.jpg', storage: :s3, url: ":s3_domain_url", styles: {
 		thumb: '100x100#'
 	}, bucket: 'designscook-assets',
 	path: "/submissions/:id/attachment/:style.:extension", s3_host_name: 's3.amazonaws.com', s3_credentials: "#{Rails.root}/config/aws.yml"
